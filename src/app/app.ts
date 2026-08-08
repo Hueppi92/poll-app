c2cd34e923ae13cb21b3cb28e9e8bc0eff821c01
Hueppi92 <a.huepgen@web.de>
2026-08-08 14:10:18 +0200

    changed spaces in header and hero component. added survey-overview-component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Hero } from './hero/hero';
+import { SurveyOverview } from './survey-overview/survey-overview';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, SurveyOverview],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/survey-overview-card/survey-overview-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-overview-card',
+  imports: [],
+  templateUrl: './survey-overview-card.html',
+  styleUrl: './survey-overview-card.scss',
+})
+export class SurveyOverviewCard {}
